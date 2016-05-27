416f692c10e00162031499eb0aae69f7c7a23001
Marcin Kruglik <blekione@gmail.com>
2016-05-27 06:21:44 +0100

    Adds logic for creating, getting and updating customer and creates interface with JAX-RS annotations

--- RestApp/src/main/java/org/krugdev/services/CustomerResource.java
+++ RestApp/src/main/java/org/krugdev/services/CustomerResource.java
@@ -1,5 +1,31 @@
 package org.krugdev.services;
 
-public class CustomerResource {
+import java.io.InputStream;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+@Path("/customers")
+public interface CustomerResource {
+
+	@POST
+	@Consumes("application/xml")
+	public Response createCustomer(InputStream in);
+	
+	@GET
+	@Path("{id}")
+	@Produces("application/xml")
+	public StreamingOutput getCustomer(@PathParam("id") int id);
+	
+	@PUT
+	@Path("{id}")
+	@Consumes("application/xml")
+	public void updateCustomer(@PathParam("id") int id, InputStream in);
 }
